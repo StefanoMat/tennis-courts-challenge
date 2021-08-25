996c3c3f42504b930e7b1d768fef3100fde4837a
Stefano Kaefer <stefanokaefer@MacBook-Pro-de-Stefano.local>
2021-08-25 11:20:00 -0300

    refactor: date and status rule on list of tennis court's schedules

--- src/main/java/com/tenniscourts/schedules/ScheduleRepository.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleRepository.java
@@ -3,10 +3,13 @@ package com.tenniscourts.schedules;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    @Query("select s from Schedule s left join Reservation r on (r.schedule.id = s.id) where s.tennisCourt.id = ?1 and r.id IS NULL order by s.startDateTime")
+    @Query("select s from Schedule s left join Reservation r on (r.schedule.id = s.id) where s.tennisCourt.id = ?1 " +
+            "and (r.id IS NULL OR r.reservationStatus <> 0) " +
+            "and s.startDateTime >= current_timestamp order by s.startDateTime")
     List<Schedule> findByTennisCourt_IdAndWithoutReservationOrderByStartDateTime(Long id);
 }
